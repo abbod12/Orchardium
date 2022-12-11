490afcc71db63cef65970983a26a281a0a1143f3
abbod12 <79143823+abbod12@users.noreply.github.com>
2022-12-12 01:43:40 +0200

    Add files via upload

---
+++ graphs/js/chartapp.js
@@ -0,0 +1,159 @@
+
+/*
+const ctx = document.getElementById('myChart');
+
+
+new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: [ '1d', '2d', '3d', '4d', '5d'],
+    datasets: [{
+      label: '# Temperature',
+      fill: true,
+      pointBackgroundColor:"#fff",
+      borderColor:"#fff",
+      data: [12, 10, 14, 16, 29, 31],
+      borderWidth: 1,
+      hitRadius:700,
+      hoverRadius:12,
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+const sec = document.getElementById('myChart2');
+
+
+new Chart(sec, {
+  type: 'line',
+  data: {
+    labels: [ '1d', '2d', '3d', '4d', '5d'],
+    datasets: [{
+      label: '# Humidity',
+      fill: true,
+      pointBackgroundColor:"#fff",
+      borderColor:"#fff",
+      data: [80, 82, 79, 84, 86, 82],
+      borderWidth: 1,
+      hitRadius:700,
+      hoverRadius:12,
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+const vix = document.getElementById('myChart3');
+
+
+new Chart(vix, {
+  type: 'line',
+  data: {
+    labels: [ '1d', '2d', '3d', '4d', '5d'],
+    datasets: [{
+      label: '# Soil Moisture',
+      fill: true,
+      pointBackgroundColor:"#fff",
+      borderColor:"#fff",
+      data: [80, 100, 70, 90, 50, 80],
+      borderWidth: 1,
+      hitRadius:700,
+      hoverRadius:12,
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+
+
+
+const vixen = document.getElementById('myChart3');
+
+
+new Chart(vixen, {
+  type: 'line',
+  data: {
+
+    datasets: [{
+
+      type: 'line',
+
+      yAxisID: 'temperature',
+
+      backgroundColor: 'transparent',
+
+      borderColor: 'rgb(255, 99, 132)',
+
+      pointBackgroundColor: 'rgb(255, 99, 132)',
+
+      tension: 0,
+
+      fill: false
+
+    }, {
+
+      yAxisID: 'precipitation',
+
+      backgroundColor: 'rgba(54, 162, 235, 0.5)',
+
+      borderColor: 'transparent'
+
+    }]
+
+  },
+
+  options: {
+
+    scales: {
+
+      yAxes: [{
+
+        id: 'temperature',
+
+        gridLines: {
+
+          drawOnChartArea: false
+
+        }
+
+      }, {
+
+        id: 'precipitation',
+
+        position: 'right',
+
+        gridLines: {
+
+          drawOnChartArea: false
+
+        }
+
+      }]
+
+    },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+}});
+*/
